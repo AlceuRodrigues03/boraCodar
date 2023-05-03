45c07c0fb6917dd6b3b47f0c92cdac9f9c63ac3e
John Everton <johneverton44@gmail.com>
2023-05-02 22:31:58 -0300

    player interface

---
+++ player/src/App.tsx
@@ -0,0 +1,10 @@
+import { Player } from "./components/Player";
+
+export function App() {
+  return (
+    <main className="h-screen flex justify-center items-center">
+      <Player />
+    </main>
+  )
+}
+
